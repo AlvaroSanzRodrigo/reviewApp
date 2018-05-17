2101073f1c8ab01bef3ab6d5dc69402d6f1577be
Alvaro Sanz Rodrigo <sanzrodrigoalvaro@gmail.com>
2018-05-18 00:37:01 +0200

    Root commit

---
+++ ReviewApp/DataHolder.swift
@@ -0,0 +1,20 @@
+//
+//  DataHolder.swift
+//  ReviewApp
+//
+//  Created by Alvaro Sanz Rodrigo on 18/05/2018.
+//  Copyright © 2018 Alvaro Sanz Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+    
+    static let sharedInstance = DataHolder();
+    
+    var hello : String = "hola"
+    
+    
+    
+
+}
