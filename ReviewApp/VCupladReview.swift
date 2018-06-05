f17e1e9a6268bb8c5849dfdb5122a7f7790a4381
Alvaro Sanz Rodrigo <alvaro.sanz@live.u-tad.com>
2018-06-05 09:52:12 +0200

    subirReview view Controller

---
+++ ReviewApp/VCupladReview.swift
@@ -0,0 +1,49 @@
+//
+//  VCupladReview.swift
+//  ReviewApp
+//
+//  Created by Álvaro Sanz Rodrigo on 5/6/18.
+//  Copyright © 2018 Alvaro Sanz Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class VCupladReview: UIViewController {
+
+    @IBOutlet weak var txtProducto: UITextField!
+    @IBOutlet weak var txtMarca: UITextField!
+    @IBOutlet weak var txtAreaDescripcion: UITextView!
+    @IBOutlet weak var txtPros: UITextField!
+    @IBOutlet weak var btnPros: UIButton!
+    @IBOutlet weak var txtContras: UITextField!
+    @IBOutlet weak var btnContras: UIButton!
+    @IBOutlet weak var txtLinkCompra: UITextField!
+    @IBOutlet weak var btnCamara: UIButton!
+    @IBOutlet weak var btnGaleria: UIButton!
+    @IBOutlet weak var sldScore: UISlider!
+    @IBAction func btnProsAccion(_ sender: Any) {
+    }
+    @IBAction func btnConsAccion(_ sender: Any) {
+    }
+    @IBAction func btnCamaraAccion(_ sender: Any) {
+    }
+    @IBAction func btnGaleriaAccion(_ sender: Any) {
+    }
+    @IBAction func btnAceptarAccion(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+
+}
